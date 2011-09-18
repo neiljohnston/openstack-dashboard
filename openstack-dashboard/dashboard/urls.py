aff6c557350ef6e73c9d44900ebc27c00fe06d7a
Neil Johnston <onewheeldrive.net@gmail.com>
2011-09-18 16:46:55 -0700

    Simple Feedback page added.

--- openstack-dashboard/dashboard/urls.py
+++ openstack-dashboard/dashboard/urls.py
@@ -37,6 +37,7 @@ urlpatterns = patterns('',
     url(r'^pistondownloads/$', 'dashboard.views.pistondownloads', name='dash_pistondownloads'),
     url(r'^pistonupdates/$', 'dashboard.views.pistonupdates', name='dash_pistonupdates'),
     url(r'^pistonexipred/$', 'dashboard.views.pistonexpired', name='dash_pistonexpired'),
+    url(r'^pistonfeedback/$', 'dashboard.views.pistonfeedback', name='dash_pistonfeedback'),
 )
 
 
