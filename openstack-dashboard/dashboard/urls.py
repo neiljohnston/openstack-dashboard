248d3167cb5d6e8d09f489b0ba90050f24edee39
Mike Lundy <mike@fluffypenguin.org>
2011-11-01 01:39:13 -0700

    typo fix

--- openstack-dashboard/dashboard/urls.py
+++ openstack-dashboard/dashboard/urls.py
@@ -39,7 +39,7 @@ urlpatterns = patterns('',
         name='syspanel_overview'),
     url(r'^pistondownloads/$', 'dashboard.views.pistondownloads', name='dash_pistondownloads'),
     url(r'^pistonupdates/$', 'dashboard.views.pistonupdates', name='dash_pistonupdates'),
-    url(r'^pistonexipred/$', 'dashboard.views.pistonexpired', name='dash_pistonexpired'),
+    url(r'^pistonexpired/$', 'dashboard.views.pistonexpired', name='dash_pistonexpired'),
     url(r'^pistonfeedback/$', 'dashboard.views.pistonfeedback', name='dash_pistonfeedback'),
 )
 
