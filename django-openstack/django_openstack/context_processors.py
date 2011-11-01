58cfa4cc88f38e3a755ade8a8ab47fa793db4ce8
Christopher MacGown <ignoti+github@gmail.com>
2011-11-01 00:40:37 -0700

    Blah blah blah

--- django-openstack/django_openstack/context_processors.py
+++ django-openstack/django_openstack/context_processors.py
@@ -116,7 +116,11 @@ def pentos(request):
     #  Entitlement Handling
     # *************************************************************
     curent_time = time.time()
-    entitlement_timestamp = getTimestamp(request, entitlement_key_path + entitlement_key_name)
+    try:
+        entitlement_timestamp = getTimestamp(request, entitlement_key_path + entitlement_key_name)
+    except OSError:
+        entitlement_timestamp = time.now()
+
     expiry_time = 90 * 24 * 60 * 60
     if entitlement_timestamp:
         entitlement_expires = entitlement_timestamp + expiry_time
@@ -124,8 +128,6 @@ def pentos(request):
     else:
         # Set Default UI behaviour for Entitlement if an OSError is thrown reaching entitlement key
         entitled = False
-
-
 
 
     # *************************************************************
