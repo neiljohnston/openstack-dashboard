23ec8a945c3508ca1dc52a122cec9178de408995
Mike Lundy <mike@fluffypenguin.org>
2012-01-16 14:41:29 -0800

    clear up depreciation warning, and disable unused template loader

--- openstack-dashboard/dashboard/settings.py
+++ openstack-dashboard/dashboard/settings.py
@@ -72,8 +72,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_DIRS = (
